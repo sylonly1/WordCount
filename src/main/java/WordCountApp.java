a33d8921641c01280edc8509e5fdd713545b43bc
shiyongliang <2534041854@qq.com>
2022-04-20 17:44:37 +0800

    WordCount 初始化

---
+++ src/main/java/WordCountApp.java
@@ -0,0 +1,78 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.StringTokenizer;
+
+public class WordCountApp {
+    public static class MyMapper
+                        extends Mapper<Object, Text, Text,IntWritable> {
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            StringTokenizer itr = new StringTokenizer(value.toString());
+            while(itr.hasMoreTokens()) {
+                word.set(itr.nextToken());
+                context.write(word, one);
+            }
+        }
+    }
+
+    public static class MyReducer
+                        extends Reducer<Text, IntWritable, Text, IntWritable> {
+        private IntWritable result = new IntWritable();
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            for(IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        String INPUT_PATH = "hdfs://localhost:8020/wc";
+        String OUTPUT_PATH = "hdfs://localhost:8020/outputwc";
+        Configuration conf = new Configuration();
+        final FileSystem fileSystem = FileSystem.get(new URI(INPUT_PATH), conf);
+        if(fileSystem.exists(new Path(OUTPUT_PATH))) {
+            fileSystem.delete(new Path(OUTPUT_PATH),true);
+        }
+
+        Job job = Job.getInstance(conf, "WordCountApp");
+        //运行jar类
+        job.setJarByClass(WordCountApp.class);
+        //设置map
+        job.setMapperClass(MyMapper.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+        //设置reduce
+        job.setReducerClass(MyReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        //设置输入格式
+        job.setInputFormatClass(TextInputFormat.class);
+        Path intputPath = new Path(INPUT_PATH);
+        FileInputFormat.addInputPath(job, intputPath);
+        //设置输出格式
+        job.setOutputFormatClass(TextOutputFormat.class);
+        Path outputPath = new Path(OUTPUT_PATH);
+        FileOutputFormat.setOutputPath(job, outputPath);
+        //提交job
+
+        System.exit(job.waitForCompletion(true)?0:1);
+    }
+}
