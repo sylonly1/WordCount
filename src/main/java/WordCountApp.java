7c418a4a1d5eca90d7819a3da88d755e2a78b223
shiyongliang <2534041854@qq.com>
2022-04-20 17:59:10 +0800

    注释

--- src/main/java/WordCountApp.java
+++ src/main/java/WordCountApp.java
@@ -33,7 +33,6 @@ public class WordCountApp {
     public static class MyReducer
                         extends Reducer<Text, IntWritable, Text, IntWritable> {
         private IntWritable result = new IntWritable();
-        //
         public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
             int sum = 0;
             for(IntWritable val : values) {
